88231e370d703dc226867fd77a08a07f546e3583
hiteshpattanayak-tw <hpattanayak@investvoyager.com>
2022-06-03 11:13:48 +0530

    Add contract for url service

--- internal/app/service/urls.go
+++ internal/app/service/urls.go
@@ -1,5 +1,27 @@
 package service
 
-type UrlService struct {
+import (
+	"github.com/hiteshpattanayak-tw/url_shortner/internal/app/repository"
+	"github.com/hiteshpattanayak-tw/url_shortner/internal/app/types"
+)
 
+type UrlService interface {
+	ShortenUrl(longUrl types.LongUrl) types.ShortUrl
+	GetOriginalUrl(shortUrl types.ShortUrl) types.LongUrl
+}
+
+type urlService struct {
+	urlRepo repository.UrlRepository
+}
+
+func ProvideUrlService(urlRepo repository.UrlRepository) UrlService {
+	return urlService{urlRepo: urlRepo}
+}
+
+func (us urlService) ShortenUrl(longUrl types.LongUrl) types.ShortUrl {
+	return ""
+}
+
+func (us urlService) GetOriginalUrl(shortUrl types.ShortUrl) types.LongUrl {
+	return ""
 }
